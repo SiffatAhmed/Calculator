beeae5ee4f74cca9b08c4969a4b7e09a360942e6
SiffatAhmed <54367251+SiffatAhmed@users.noreply.github.com>
2020-07-23 16:55:26 +0500

    Add files via upload

---
+++ TaxCalculator/TaxCalculator/Calculator.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace TaxCalculator
+{
+    partial class Calculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.calculatortype = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calculatortype
+            // 
+            this.calculatortype.AutoSize = true;
+            this.calculatortype.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculatortype.Location = new System.Drawing.Point(292, 9);
+            this.calculatortype.Name = "calculatortype";
+            this.calculatortype.Size = new System.Drawing.Size(105, 21);
+            this.calculatortype.TabIndex = 0;
+            this.calculatortype.Text = "Tax Calculator";
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(255)))), ((int)(((byte)(236)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.calculatortype);
+            this.Name = "Calculator";
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label calculatortype;
+    }
+}
+
